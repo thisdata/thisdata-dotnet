b7e4263254e7c4d2998ee8339158973b03f45eed
Rich Chetwynd <richardchetwynd@gmail.com>
2016-04-15 21:13:25 +1200

    switched to nunit

--- tests/ClientTests.cs
+++ tests/ClientTests.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 using ThisData;
 
 namespace ThisData.Net.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class ClientTests
     {
         string signature = "291264d1d4b3857e872d67b7587d3702b28519a0e3ce689d688372b7d31f6af484439a1885f21650ac073e48119d496f44dc97d3dc45106409d345f057443c6b";
         string payload = "{\"version\":1,\"was_user\":null,\"alert\":{\"id\":533879540905150463,\"description\":null}}";
 
-        [TestMethod]
+        [Test]
         public void ValidateWebhook_WithValidSecret()
         {
             var client = new ThisData.Client("");
@@ -20,7 +20,7 @@ namespace ThisData.Net.Tests
             Assert.IsTrue(client.ValidateWebhook(secret, signature, payload));
         }
 
-        [TestMethod]
+        [Test]
         public void ValidateWebhook_WithInvalidSecret()
         {
             var client = new ThisData.Client("");
